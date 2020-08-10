2b201847917a4b06dc21e0dd7060c11bbecf0a1e
Maxime Pierron <maxime.pierron@outlook.com>
2020-08-10 16:28:19 +0200

    mistake in event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,8 @@ class Event < ApplicationRecord
     DATE_FORMAT = /\d{2}\/\d{2}\/\d{4}/
     validates :start_date, presence: true, format: { with: DATE_FORMAT }
     validates :duration, presence: true
-    validates : title, length:{minimum:10}
-    validates :description, length:{minimum: 100}
+    validates :title, presence: true, length:{minimum:10}
+    validates :description, presence: true, length:{minimum: 100}
     validates :price, presence: true
     validates :location, presence: true
     
