0ba782d553979c1f2d4222b0ab03393e0b3aaa38
Maxime Pierron <maxime.pierron@outlook.com>
2020-08-10 15:54:40 +0200

    email, password added to user model remove migration

--- db/migrate/20200810130405_add_email_password_to_user_readyfor_devise.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailPasswordToUserReadyforDevise < ActiveRecord::Migration[5.2]
-  def change
-    t.string :email,              null: false, default: ""
-    t.string :encrypted_password, null: false, default: ""
-  end
-end
