3ef5725552450f621c90c141692c97ef16f05a9c
Maxime Pierron <maxime.pierron@outlook.com>
2020-08-11 16:42:11 +0200

    index

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+	def new
+    @user = User.new
+    end
+
+    def create
+
+        @user = User.new(first_name: params[:first_name],
+        last_name: params[:last_name],
+        description: params[:description],
+        email: params[:email],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
+        )
+
+        if @user.save
+            flash[:success] = "Bienvenue #{@user.first_name}!"
+            log_in(@user)
+            redirect_to root_path #Affiche l'index des gossips
+        else
+            render 'new' # Reste sur la view de gossips New.
+        end   
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def index
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
